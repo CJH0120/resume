ba20f24f35c8ef66eac98876cc258cc91f087182
최종현 <jonghyun@choejonghyeon-ui-Macmini.local>
2024-08-16 14:41:08 +0900

    RESET RESUME

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,5 @@ import Image from "next/image"
 import styles from "./page.module.scss"
 import image from "@/public/tesing.jpg"
 export default function Home() {
-	return (
-		<main className={styles.main}>
-			<p className={styles.description}>CSS테스트</p>
-			<Image
-				src={image.src}
-				alt="Vercel Logo"
-				width={image.width}
-				height={image.height}
-			/>
-		</main>
-	)
+	return <main className={styles.main}></main>
 }
